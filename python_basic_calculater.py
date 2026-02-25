8159ccc815fe72da57e7631828c52cf67b7f516a
Mohd Maviya <MOHDMAVIYA425@GMAIL.COM>
2026-02-25 14:35:43 +0530

    Add files via upload

---
+++ python_basic_calculater.py
@@ -0,0 +1,36 @@
+#python calculater 
+a = int(input("enter the first number:"))
+b = int(input("enter the second number:"))
+operator = input("enter the operator:")
+if operator == '+':
+    print("the addition of a+b:",a+b)
+elif operator == '-':
+    print("the subtraction of a-b:",a-b)
+elif operator == '*':
+    print("the multiplication of a*b:",a*b)
+elif operator == '/':
+    print("the division of a/b:",a/b)
+elif operator == '%':
+    print("the modulus of a%b:",a%b)
+else:
+    print("invalid operator:")
+
+
+#basic arithmetic_operator calculator 
+
+num1=float(input("enter the no 1: "))
+num2=float(input("enter the no 2: "))
+operator=input("enter the operator: ")
+
+if operator=='+':
+    print(num1+num2)
+elif operator=='-':
+    print(num1-num2)
+elif operator=='*':
+    print(num1*num2)
+elif operator=='/':
+    print(num1/num2)
+elif operator=='%':
+    print(num1%num2)
+else:
+    print("invalid number: ")
