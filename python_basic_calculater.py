089e1bb9e914c238d98196a356d2c7a2b6829ce8
MAVIYA877 <mohdmaviya425@gmail.com>
2026-04-13 12:38:01 +0530

    new modified

--- python_basic_calculater.py
+++ python_basic_calculater.py
@@ -16,21 +16,3 @@ else:
     print("invalid operator:")
 
 
-#basic arithmetic_operator calculator 
-
-num1 = float(input("enter the no 1: "))
-num2 = float(input("enter the no 2: "))
-operator = input("enter the operator: ")
-
-if operator=='+':
-    print(num1+num2)
-elif operator=='-':
-    print(num1-num2)
-elif operator=='*':
-    print(num1*num2)
-elif operator=='/':
-    print(num1/num2)
-elif operator=='%':
-    print(num1%num2)
-else:
-    print("invalid number: ")
