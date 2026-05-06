c14ae54b28747bab557ecdbf079111b5a37e81c7
MAVIYA877 <mohdmaviya425@gmail.com>
2026-05-06 14:25:04 +0530

    add code

---
+++ arthmetic_operation.py
@@ -0,0 +1,17 @@
+#========addition,subtraction,multiplication,division,modulus========#
+a = int(input("enter the number of a:"))
+b = int(input("enter the number of b:"))
+
+print("the addtion of a+b =",a+b)
+print("the subtraction of a-b =",a-b)
+print("the multiplication of a*b = ",a*b)
+print("the division  of a/b =",a/b)
+print("the modulus of a%b =",a%b)
+
+
+a = float(input("enter the a:"))
+b = float(input("enter the b:"))
+c = a-b
+d = c
+print(d)
+print(c)
