5223a4140a0c2589f73b617c595f16f2ac79d9f7
MAVIYA877 <mohdmaviya425@gmail.com>
2026-05-07 21:39:07 +0530

    code add

---
+++ hello_world-user_input-operator.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+#user input 
+a = int(input("enter the number of b:"))
+b = int(input("enter the number of b:"))
+
+#operator 
+a = 10 
+b = 20
+
+print("addtion of a+b =",a+b)
+print("subtraction of a-b =",a-b)
+print("multiplication of a*b =",a*b)
+print("division of a/b =",a/b)
